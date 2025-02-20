d063bf63ee4bb19348afb950027c81e61a18c7e8
ayeshamukta <ayeshamukta0000@gmail.com>
2025-02-21 01:31:52 +0600

    prepared necessary tools

---
+++ src/Components/header/Header.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <p className="text-5xl">Tailwind Connected</p>
+        </div>
+    );
+};
+
+export default Header;
